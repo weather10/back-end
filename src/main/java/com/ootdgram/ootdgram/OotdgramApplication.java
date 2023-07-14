ee11adba6650560acd831943ce778d3cb0b0473a
kki4504 <rhkddlf9876@gmail.com>
2023-07-14 17:13:40 +0900

    init commit

---
+++ src/main/java/com/ootdgram/ootdgram/OotdgramApplication.java
@@ -0,0 +1,13 @@
+package com.ootdgram.ootdgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OotdgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OotdgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ootdgram/ootdgram/OotdgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ootdgram.ootdgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OotdgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
