ce860a61803ea8c6b2518d4b294f16fc165e5839
kki4504 <rhkddlf9876@gmail.com>
2023-07-14 20:07:33 +0900

    Feature: repository / service

---
+++ src/main/java/com/ootdgram/ootdgram/service/UserService.java
@@ -0,0 +1,10 @@
+package com.ootdgram.ootdgram.service;
+
+import com.ootdgram.ootdgram.domain.dto.SignupRequestDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public void signup(SignupRequestDto requestDto) {
+    }
+}
