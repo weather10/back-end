1338bc19995c11c464afb903a39c560ed2a70d44
kki4504 <rhkddlf9876@gmail.com>
2023-07-15 14:36:26 +0900

    feat: pattern 정규식 추가

--- src/main/java/com/ootdgram/ootdgram/domain/dto/SignupRequestDto.java
+++ src/main/java/com/ootdgram/ootdgram/domain/dto/SignupRequestDto.java
@@ -1,16 +1,20 @@
 package com.ootdgram.ootdgram.domain.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class SignupRequestDto {
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9]{5,12}$")
     private String name;
 
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")
     private String email;
 
     @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,21}$")
     private String password;
 }
