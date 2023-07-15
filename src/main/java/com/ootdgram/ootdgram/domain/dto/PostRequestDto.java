eb2466ace5226e3871016632f4e342dc6614367a
jungeun <pom909@naver.com>
2023-07-15 13:13:55 +0900

    feat : post 뼈대

---
+++ src/main/java/com/ootdgram/ootdgram/domain/dto/PostRequestDto.java
@@ -0,0 +1,11 @@
+package com.ootdgram.ootdgram.domain.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PostRequestDto {
+    private String image;
+    private String content;
+
+//    weather 보류
+}
