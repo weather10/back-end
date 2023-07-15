41608c87e8faf7c2b7494b24872c4b62e14efffb
stoow1 <spacegle12341@gmail.com>
2023-07-15 07:59:24 -0400

    Feature: JwtUtil

--- src/main/java/com/ootdgram/ootdgram/OotdgramApplication.java
+++ src/main/java/com/ootdgram/ootdgram/OotdgramApplication.java
@@ -2,8 +2,11 @@ package com.ootdgram.ootdgram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@EnableJpaAuditing
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class OotdgramApplication {
 
     public static void main(String[] args) {
