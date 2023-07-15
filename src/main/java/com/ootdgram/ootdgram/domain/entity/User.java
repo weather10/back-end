cbb1e58129899f0ce4f0a78253c16a0b3dee7263
kki4504 <rhkddlf9876@gmail.com>
2023-07-15 14:29:32 +0900

    feat: User Entity 생성자

--- src/main/java/com/ootdgram/ootdgram/domain/entity/User.java
+++ src/main/java/com/ootdgram/ootdgram/domain/entity/User.java
@@ -2,9 +2,11 @@ package com.ootdgram.ootdgram.domain.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class User extends Timestamped{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,6 +19,12 @@ public class User extends Timestamped{
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 }
