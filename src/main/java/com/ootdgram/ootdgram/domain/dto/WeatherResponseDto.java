b560ed88e817c9534f478b19aab9b6a3597246be
jungeun <pom909@naver.com>
2023-07-20 09:28:17 +0900

    feat : yml timestemped korea time"

--- src/main/java/com/ootdgram/ootdgram/domain/dto/WeatherResponseDto.java
+++ src/main/java/com/ootdgram/ootdgram/domain/dto/WeatherResponseDto.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Getter
 @Builder
-public class WeatherResponseDto {//강수량, 풍속1, 습도1, 주소, 온도1, 날짜1 ,기상상태1
+public class WeatherResponseDto {
     // 현재 온도
     private double currentTemperature;
     // 기상상태
@@ -19,16 +19,9 @@ public class WeatherResponseDto {//강수량, 풍속1, 습도1, 주소, 온도1,
     private double humidity;
     // 풍속
     private double windSpeed;
-    // 주소 //Geocoding API?//
+    // 주소 //Geocoding API
     private String address;
-    // 강수량 // 현재날씨에는 1h 단위, 예보날씨에는 3h 단위?? 통일 해줘야하나.  //!!!!! 문제 발생  비안올때는 강수량 필드가 없이 응답해서 에러발생!
-    /*
-        if (jsonObject.has("rain")) {
-            log.info("강수량 = " + jsonObject.getAsJsonObject("rain").get("1h").getAsDouble());
-        } else {
-            log.info("강수량 정보가 없습니다.");
-        } 추가해야함
-        확인후 삭제*/ // 넹
+    // 강수량
     private double precipitation;
     // 날짜
     private LocalDateTime dateTime;
@@ -49,38 +42,3 @@ public class WeatherResponseDto {//강수량, 풍속1, 습도1, 주소, 온도1,
         }
     }
 }
-
-//        public forecastWeather(ForecastWeatherResponseDto fo) {
-//            this.forecastTempAvg = WeatherUtil.tempAvg(fo.getList());
-//            this.weatherStatus = WeatherUtil.getWeather(fo.getList());
-//            this.dateTime = WeatherUtil.getDateTime(fo.getList());
-//        }
-//    }
-
-// 넘겨줘야하는 dto 데이터, 표시되는 데이터는 현재날씨 종합 , 3일? 5일? 날씨 예보 // 넘길때 한번에 넘기되, 현재와 예보 날씨를 둘다 넘겨서, 어떻게 담을지??
-//
-//    {
-//    currentTemperature: ㅁㅁㅁ,
-//    weatherStatus: ㅁㅁㅁ,
-//    weatherStatus: ㅁㅁㅁ,
-//    humidity: ㅁㅁㅁ,
-//    windSpeed: ㅁㅁㅁ,
-//    address: ㅁㅁㅁ,
-//    precipitation: ㅁㅁㅁ,
-//    dateTime: ㅁㅁㅁ
-//    list{  // 예보날씨 리스트
-//          forecastTempAvg :aa,
-//          weatherStatus : aaa,
-//          dateTime:
-//         }
-//    }
-//    public WeatherResponseDto(CurrentWeatherResponseDto currentWeather,
-//                              ForecastWeatherResponseDto forecastWeather) {
-//        this.currentTemperature = currentWeather.getMain().getTemp();
-//        this.weatherStatus = currentWeather.getWeather().get(0).getMain();
-//        this.humidity = currentWeather.getMain().getHumidity();
-//        this.windSpeed = currentWeather.getWind().getSpeed();
-//        this.address = null;
-//        this.precipitation = currentWeather.getRain().getH1();
-//        this.dateTime = WeatherUtil.convertTimestamp(currentWeather.getDt());
-//        this.forecastWeatherList =
